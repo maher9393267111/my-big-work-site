94ad6cd6a2209e4d8cf4a2bf99688c8aecf4446b
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-05 18:41:18 +0300

    satrat prjects page

---
+++ pages/projects.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProjectsBanner from '../components/projectsComp/projectBanner'
+import Layout from '../components/Layout/index'
+
+export default function Projects() {
+  return (
+    <Layout>
+
+
+<ProjectsBanner/>
+
+
+
+
+
+    </Layout>
+  )
+}
