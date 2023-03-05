4565d2840e64cccd53edc68f66342feb729a3e36
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-05 18:49:20 +0300

    porjects try filter

---
+++ components/projectsComp/projectSection.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+
+export default function ProjectsSection() {
+  return (
+    <>
+    
+    <section class="portfolio-gallery s-padding">
+<div class="container">
+<div class="s-title-wrap">
+<span class="s-sub-title">Our Portfolio</span>
+<h2 class="s-title">Some of Our Finest Work</h2>
+</div>
+<div class="row">
+<div class="col-12">
+<div class="porftolio-filter-menu">
+<div class="portfolio-filter-buttons text-center" id="filter-buttons">
+<button class="filter-btn current" data-filter="*">
+All Categories
+</button>
+<button class="filter-btn" data-filter=".software">
+Software
+</button>
+<button class="filter-btn" data-filter=".design">Design</button>
+<button class="filter-btn" data-filter=".cyber-security">
+Cyber Security
+</button>
+<button class="filter-btn" data-filter=".engineering">
+Engineering
+</button>
+<button class="filter-btn" data-filter=".consulting">
+Consulting
+</button>
+</div>
+</div>
+</div>
+</div>
+<div class="row portfolio-gallery-container">
+<div class="portfolio-item software">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-1.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item software">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-2.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item design">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-4.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item cyber-security">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-3.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item consulting">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-5.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item software">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/porftolio-img-6.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="portfolio-item engineering">
+<div class="portfolio-item__inner">
+<div class="thumb">
+<img src="assets/images/portfolio-thumb/portfolio-img-7.png" alt="" />
+</div>
+<div class="content">
+<div class="p-left">
+<h3>Colorful Small Cards</h3>
+<span>Branding</span>
+</div>
+<div class="p-right">
+<a href="portfolio-detail.html"><i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+    
+    
+    </>
+  )
+}
--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ProjectsBanner from '../components/projectsComp/projectBanner'
 import Layout from '../components/Layout/index'
+import ProjectsSection from '../components/projectsComp/projectSection'
 
 export default function Projects() {
   return (
     <Layout>
 
 
+
 <ProjectsBanner/>
+
+<ProjectsSection/>
 
 
 
