fc093c0d4ea9c3c30a2764634355ac215cceca91
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-05 18:04:20 +0300

    add pricing page

---
+++ components/priceComponents/priceBanner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function PriceBanner() {
+  return (
+    <>
+    <section className="d-page-banner">
+<div className="container">
+<div className="row">
+<div className="content">
+<h1>Pricing</h1>
+<ul className="breadcrumb">
+<li><a href="#">Home</a></li>
+<li><a href="pricing.html">Pricing</a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+    
+    
+    </>
+  )
+}
---
+++ pages/pricing.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PriceBanner from '../components/priceComponents/priceBanner'
+import PriceCards from '../components/priceComponents/priceCards'
+import Layout from '../components/Layout'
+
+export default function Pricing() {
+  return (
+    <Layout>
+
+<div>
+
+
+<PriceBanner/>
+
+
+<PriceCards/>
+
+
+</div>
+
+
+
+    </Layout>
+  )
+}
