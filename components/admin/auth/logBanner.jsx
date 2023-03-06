41825ae3e922b33b1dc6b80e8bf635f632305e4f
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-06 11:57:29 +0300

    try login ???

--- components/admin/auth/logBanner.jsx
+++ components/admin/auth/logBanner.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function LogBanner() {
   return (
     <>
+    
     <section className="d-page-banner">
 <div className="container">
 <div className="row">
