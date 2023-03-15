266afa96b3b4dca25e7d430736ea2a7e5b0f1bb7
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-15 12:11:14 +0300

    add

--- components/about/history.jsx
+++ components/about/history.jsx
@@ -70,6 +70,10 @@ alt="" />
 <div className="p-right">
 <img src="https://images.unsplash.com/photo-1519241047957-be31d7379a5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
 </div>
+
+
+
+
 </div>
 </div>
 </div>
