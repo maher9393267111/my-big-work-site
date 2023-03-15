252879a56a9f0187f38cec76c53fb1f75afc96c3
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-15 12:15:30 +0300

    addd

--- pages/admin/projects.jsx
+++ pages/admin/projects.jsx
@@ -6,22 +6,7 @@ import AdminProjectsComponent from '../../components/admin/projects/allProjects'
 export default function AdminProjects() {
   return (
     <div>
-<Head>
 
-<Script
-onLoad={()=>console.log('LOADED PLUGINS FILE 🔸️🔷️🔶️▫️')}
-
-src="/assets/js/plugins.js" async   strategy='lazyOnload' ></Script>
-
-
-
-
-<Script 
-
-onLoad={()=>console.log('LOADED MAIN FILE 🔸️🔷️🔶️▫️')}
-type="text/javascript" src="/assets/js/main.js" async  strategy='lazyOnload'   ></Script>
-
-</Head>
 
 
 <AdminProjectsComponent/>
