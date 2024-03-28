32470654ef48ea40dfb4b9e7f010919808e82008
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-03-28 15:10:50 +0300

    fix filter

--- components/projectsComp/projectSection.jsx
+++ components/projectsComp/projectSection.jsx
@@ -1,90 +1,145 @@
-import React from 'react'
+import React ,{useState , useEffect} from "react";
 
-
-
-export default function ProjectsSection({data:projects}) {
-
-
-
+export default function ProjectsSection({ data: projects }) {
   const description = (str) => {
-    if ((str === null) || (str === '')) {
-        return '';
+    if (str === null || str === "") {
+      return "";
     } else {
-        str = str.toString();
+      str = str.toString();
     }
-    const newStr = str.replace(/(<([^>]+)>)/gi, '')
-    return newStr.length >= 150 ? newStr.slice(0, 150) + "..." : newStr
-}
-const title = (data) => {
-    return data.length >= 50 ? data.slice(0, 50) + "..." : data
-}
+    const newStr = str.replace(/(<([^>]+)>)/gi, "");
+    return newStr.length >= 150 ? newStr.slice(0, 150) + "..." : newStr;
+  };
+  const title = (data) => {
+    return data.length >= 50 ? data.slice(0, 50) + "..." : data;
+  };
+
+
+const [type ,setType] = useState('all')
+// const [selectedProjects ,setSelectedProjects] = useState(projects ?? [])
+
+// const handleFilteringPlantsByType = () => {
+//   let tempPlants = [...projects];
+//   if (type !== 'all') {
+//     tempPlants = tempPlants.filter(plant => plant.type === type);
+//   }
+//   console.log("filtere-->" ,tempPlants)
+//   setSelectedProjects(tempPlants);
+//   return tempPlants;
+// };
+
+// useEffect(() => {
+//   handleFilteringPlantsByType();
+// }, [type]);
 
 
 
+const filteredProjects = type === 'all' ? projects : projects.filter(plant => plant.type === type)
+
+// travel shop realstate personal  agency kargo 
 
   return (
     <>
-
       <section className="portfolio-gallery s-padding">
         <div className="container">
           <div className="s-title-wrap">
             {/* <span className="s-sub-title">Our Portfolio</span> */}
-            <h2 className="s-title   font-arabic">
-بعض النماذج من أعمالنا
+            <h2 className="s-title   font-arabic">بعض النماذج من أعمالنا</h2>
 
-            </h2>
+            {type}
           </div>
           <div className="row">
             <div className="col-12">
               <div className="porftolio-filter-menu">
-                <div className="portfolio-filter-buttons text-center" id="filter-buttons">
-                  <button className="filter-btn current" data-filter="*">
-                    All Categories
+                <div
+                  className="portfolio-filter-buttons text-center"
+                  id="filter-buttons"
+                >
+                  <button
+                   data-filter="*"
+                  onClick={()=>setType('all')}
+                  className="filter-btn current" >
+                  كافة المواقع
                   </button>
-                  <button className="filter-btn" data-filter=".javascript">
-                    Javascript
-                      </button>
-                 
-                  <button className="filter-btn" data-filter=".reactjs">
-                    Reactjs
+                  <button
+                   data-filter=".travel"
+                  onClick={()=>setType('travel')}
+                  className="filter-btn" >
+                    مواقع سياحة
                   </button>
-                  <button className="filter-btn" data-filter=".nextjs">
-                  Nextjs
+
+                  <button
+                   data-filter=".realstate"
+                   onClick={()=>setType('realstate')}
+                  
+                  className="filter-btn" >
+                   مواقع عقارات
                   </button>
+
+                  <button
+                   data-filter=".shop"
+                   onClick={()=>setType('shop')}
+                  
+                  className="filter-btn" >
+                  متاجر وعرض منتجات
+                  </button>
+
+                  <button
+                   data-filter=".kargo"
+                   onClick={()=>setType('kargo')}
+                  className="filter-btn" >
+                    مواقع شركات شحن ونقل
+                  </button>
+
+
+                  <button
+                        data-filter=".personal"
+                   onClick={()=>setType('personal')}
+                  
+                  className="filter-btn" >
+                 مواقع شخصية
+                  </button>
+
+                  <button
+                  data-filter=".agency"
+                   //onClick={()=>setType('agency')}
+                  //  data-filter=".reactjs"
+                  className="filter-btn" >
+         مواقع تعريفية للشركات
+                  </button>
+
                 </div>
               </div>
             </div>
           </div>
+          {/* ${project?.type} */}
           <div className="row portfolio-gallery-container">
+            {filteredProjects?.map((project, index) => (
+              <div className={`portfolio-item ${project?.type}  `} key={index}>
+                <div className="portfolio-item__inner">
+                  <div className="thumb">
+                    <img
+                      className="w-full h-[430px] md:h-[330px]"
+                      src={project?.image}
+                      alt=""
+                    />
+                  </div>
+                  <div className="content">
+                    <div className="p-left">
+                      <h3>{project?.title}</h3>
+                      {/* <span>Branding</span> */}
 
-
-          {projects.map((project ,  index) => (
-
-
-<div className={`portfolio-item ${project?.type}`} key={index}>
-<div className="portfolio-item__inner">
-  <div className="thumb">
-    <img className='w-full h-[430px] md:h-[330px]' src={project?.image} alt="" />
-  </div>
-  <div className="content">
-    <div className="p-left">
-      <h3>{project?.title}</h3>
-      {/* <span>Branding</span> */}
-
-<h3>      {description(project?.description)}...</h3>
-
-    </div>
-    <div className="p-right">
-      <a href={project?.link}><i className="fa-solid fa-arrow-right"></i></a>
-    </div>
-  </div>
-</div>
-</div>
-
-
-
-          ))}
-
+                      <h3> {description(project?.description)}...</h3>
+                    </div>
+                    <div className="p-right">
+                      <a href={project?.link}>
+                        <i className="fa-solid fa-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
 
             {/* <div className="portfolio-item software">
               <div className="portfolio-item__inner">
@@ -102,17 +157,9 @@ const title = (data) => {
                 </div>
               </div>
             </div> */}
-
           </div>
         </div>
       </section>
-
-
-
-
-
-
-
     </>
-  )
+  );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,92 +1,54 @@
-import React from 'react'
-import ProjectsBanner from '../components/projectsComp/projectBanner'
-import Layout from '../components/Layout/index'
-import ProjectsSection from '../components/projectsComp/projectSection'
-import ServiceSlider from '../components/projectsComp/serviceSlider'
-import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import React from "react";
+import ProjectsBanner from "../components/projectsComp/projectBanner";
+import Layout from "../components/Layout/index";
+import ProjectsSection from "../components/projectsComp/projectSection";
+import ServiceSlider from "../components/projectsComp/serviceSlider";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import { getAnalytics, logEvent } from "firebase/analytics";
-export default function Projects({data}) {
+export default function Projects({ data }) {
+  console.log("data", data, data?.length);
 
-console.log('data' , data   , data?.length)
-
-
-
- 
-useEffect(() => {
-  const analytics = getAnalytics();
-  logEvent(analytics, "screen_Projects", {
-    firebase_screen: "Projects",
-  });
-}, []);
-
-
-
+  useEffect(() => {
+    const analytics = getAnalytics();
+    logEvent(analytics, "screen_Projects", {
+      firebase_screen: "Projects",
+    });
+  }, []);
 
   return (
     <Layout>
+      <ProjectsBanner />
 
+      <ProjectsSection data={data} />
 
-
-
-
-
-
-<ProjectsBanner/>
-
-<ProjectsSection data={data}/>
-
-<ServiceSlider/>
-
-
-
-
-
+      <ServiceSlider />
     </Layout>
-  )
+  );
 }
 
-
-
-
-
-Projects.getInitialProps = async (context  ) => {
-  
+Projects.getInitialProps = async (context) => {
   // console.log('Query', context?.query?.country)
   const data = [];
 
-
   try {
-    const querySnapshot = await getDocs(collection(db, 'project'));
+    const querySnapshot = await getDocs(collection(db, "project"));
 
     querySnapshot.forEach((doc) => {
-      console.log('doc' , doc)
+      console.log("doc", doc);
       data.push({
         id: doc.id,
-      
-        ...doc.data()
-      
+
+        ...doc.data(),
       });
     });
   } catch (error) {
     console.error(error);
   }
 
- 
-
-
-
-
-
-
   return {
     data,
   };
 };
-
-
-       
-  
-
